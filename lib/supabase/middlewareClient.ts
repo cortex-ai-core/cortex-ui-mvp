6862f048e8279b393ceed6a6272ec98addf49a4b
Chan Phouthavong <chanphouthavong@Chans-MacBook-Pro-2.local>
2026-04-21 22:47:31 -1000

    remove supabase types middleware final

--- lib/supabase/middlewareClient.ts
+++ lib/supabase/middlewareClient.ts
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createServerClient } from "@supabase/ssr";
-import { Database } from "@/types/supabase";
 
 export function createMiddlewareSupabaseClient(
   req: NextRequest,
   res: NextResponse
 ) {
-  let supabase = createServerClient<Database>(
+  let supabase = createServerClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
@@ -17,7 +16,7 @@ export function createMiddlewareSupabaseClient(
             value: c.value,
           }));
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             res.cookies.set(name, value, options);
           });
@@ -28,4 +27,3 @@ export function createMiddlewareSupabaseClient(
 
   return supabase;
 }
-
